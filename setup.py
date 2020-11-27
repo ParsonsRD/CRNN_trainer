a9c8818103c39208bbf6f9d8fe477531212f29a5
parsonsrd <parsonsrd@gmail.com>
2020-11-27 10:45:02 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RNN',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='None',
+    author='parsonsrd',
+    author_email='',
+    description=''
+)
